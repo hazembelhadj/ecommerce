0276e24dbe6a587abb8d60401dd9b9acbd9b993a
Hazem <hazem.belhadj222@gmail.com  git config --global user.name Hazem  git config --global user.email hazem.belhadj222@gmail.com>
2021-07-23 17:07:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+    <header>
+      <a href="/">React Shopping Cart</a>
+    </header>
+    <main>
+      <div className="content">
+        <div className="main">
+          
+        </div>
+        <div className="sidebar">
+          <Cart />
+        </div>
+      </div>
+    </main>
+    <footer>All right is reserved.</footer>
+  </div>
+  );
+}
+
+export default App;
